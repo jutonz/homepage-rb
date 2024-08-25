e32d28f4726c4d95dc578651324056621d2725a8
Justin Toniazzo <jt@thoughtbot.com>
2024-08-25 10:47:34 -0400

    Reschedule todoist tasks
    If a task has the `@rollable` label, reschuled its due date to tomorrow
    at the end of each day.

---
+++ spec/models/todoist/api/tasks_spec.rb
@@ -0,0 +1,58 @@
+require "rails_helper"
+
+RSpec.describe Todoist::Api::Tasks do
+  describe ".from_json" do
+    it "converts json into an object" do
+      json = {
+        "assignee_id" => "123",
+        "assigner_id" => "123",
+        "content" => "123",
+        "creator_id" => "123",
+        "description" => "123",
+        "id" => "123",
+        "is_completed" => "123",
+        "priority" => "123",
+        "project_id" => "123"
+      }
+
+      result = described_class.from_json(json)
+
+      expect(result).to eql(described_class::Task.new(
+        assignee_id: "123",
+        assigner_id: "123",
+        content: "123",
+        creator_id: "123",
+        description: "123",
+        id: "123",
+        is_completed: "123",
+        priority: "123",
+        project_id: "123"
+      ))
+    end
+  end
+
+  describe ".rollable" do
+    it "returns rollable tasks due today" do
+      task = build(:todoist_api_task)
+      TodoistApiMocks.mock_tasks_rollable(task)
+
+      result = described_class.rollable
+
+      expect(result).to eql([task])
+    end
+  end
+
+  describe ".update" do
+    it "updates a task" do
+      task = build(:todoist_api_task, priority: 4)
+      TodoistApiMocks.mock_task_update(
+        task,
+        result: build(:todoist_api_task, id: task.id, priority: 1)
+      )
+
+      result = described_class.update(task.id, {"priority" => 1})
+
+      expect(result.priority).to eql(1)
+    end
+  end
+end
