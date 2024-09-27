53d59d6be6760564cadc3fb9aeade2598e380236
Justin Toniazzo <jutonz@hey.com>
2024-09-27 14:00:08 -0400

    Create Todo::Room record (#72)
    * Create Todo::Room record
    
    Also setup `annotate` and `shoulda-matchers` gems.
    
    This model isn't used anywhere yet but this setup was getting large so I
    wanted to PR it!
    
    * lint

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id            :bigint           not null, primary key
+#  access_token  :string           not null
+#  email         :string           not null
+#  refresh_token :string           not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  foreign_id    :string           not null
+#
+# Indexes
+#
+#  index_users_on_email       (email) UNIQUE
+#  index_users_on_foreign_id  (foreign_id) UNIQUE
+#
 class User < ActiveRecord::Base
 end
--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id            :bigint           not null, primary key
+#  access_token  :string           not null
+#  email         :string           not null
+#  refresh_token :string           not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  foreign_id    :string           not null
+#
+# Indexes
+#
+#  index_users_on_email       (email) UNIQUE
+#  index_users_on_foreign_id  (foreign_id) UNIQUE
+#
 FactoryBot.define do
   factory(:user) do
     sequence(:email) { "user#{_1}@exmaple.com" }
