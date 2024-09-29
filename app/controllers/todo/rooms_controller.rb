20bbcbe8cd2a4d2077279bb827a1d9c0151aa7b7
Justin Toniazzo <jt@thoughtbot.com>
2024-09-29 00:13:57 -0400

    Some more room crud actions
    Added show, update, and destroy.

--- app/controllers/todo/rooms_controller.rb
+++ app/controllers/todo/rooms_controller.rb
@@ -20,10 +20,37 @@ module Todo
       end
     end
 
+    def show
+      @room = find_room
+    end
+
+    def edit
+      @room = find_room
+    end
+
+    def update
+      @room = find_room
+
+      if @room.update(room_params)
+        redirect_to(todo_room_path(@room), notice: "Updated room")
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      find_room.destroy
+      redirect_to(todo_rooms_path, notice: "Deleted room")
+    end
+
     private
 
     def room_params
       params.require(:todo_room).permit(:name)
+    end
+
+    def find_room
+      current_user.todo_rooms.find(params[:id])
     end
   end
 end
--- spec/system/todo/rooms_spec.rb
+++ spec/system/todo/rooms_spec.rb
@@ -6,11 +6,43 @@ RSpec.describe "Todo rooms" do
     login_as(user)
 
     visit(todo_rooms_path)
-    click_link("New Room")
+    expect(page).to have_text("No rooms")
 
+    click_link("New Room")
     fill_in("Name", with: "room name")
     click_button("Create Room")
 
     find("[data-role=room]", text: "room name")
+    expect(page).not_to have_text("No rooms")
+  end
+
+  it "allows editing a room" do
+    user = create(:user)
+    room = create(:todo_room, user:, name: "before")
+    login_as(user)
+
+    visit(todo_room_path(room))
+
+    click_link("Edit")
+    fill_in("Name", with: "after")
+    click_button("Update Room")
+
+    find("[data-role=room-name]", text: "after")
+  end
+
+  it "allows deleting a room" do
+    user = create(:user)
+    room = create(:todo_room, user:)
+    login_as(user)
+
+    visit(todo_room_path(room))
+
+    accept_confirm do
+      click_button("Delete")
+    end
+
+    expect(page).not_to have_css(
+      "[data-role=room]", text: room.name
+    )
   end
 end
