c6b078b1d4e8498f94fa90a82a8a4937e6b8d4d4
Justin Toniazzo <jt@thoughtbot.com>
2024-09-29 00:20:20 -0400

    more tests

--- spec/requests/todo/rooms_controller_spec.rb
+++ spec/requests/todo/rooms_controller_spec.rb
@@ -15,6 +15,54 @@ RSpec.describe Todo::RoomsController do
     end
   end
 
+  describe "create" do
+    it "redirects" do
+      user = create(:user)
+      login_as(user)
+      params = {todo_room: {name: "hi"}}
+
+      post(todo_rooms_path, params:)
+
+      expect(response).to redirect_to(todo_rooms_path)
+    end
+
+    it "shows error messages" do
+      user = create(:user)
+      login_as(user)
+      params = {todo_room: {name: ""}}
+
+      post(todo_rooms_path, params:)
+
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(page).to have_text("can't be blank")
+    end
+  end
+
+  describe "update" do
+    it "redirects" do
+      user = create(:user)
+      room = create(:todo_room, user:)
+      login_as(user)
+      params = {todo_room: {name: "after"}}
+
+      put(todo_room_path(room), params:)
+
+      expect(response).to redirect_to(todo_room_path(room))
+      expect(room.reload).to have_attributes(params[:todo_room])
+    end
+
+    it "shows error messages" do
+      user = create(:user)
+      login_as(user)
+      params = {todo_room: {name: ""}}
+
+      post(todo_rooms_path, params:)
+
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(page).to have_text("can't be blank")
+    end
+  end
+
   def have_room(room)
     have_css("[data-role=room]", text: room.name)
   end
