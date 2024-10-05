c29ed5a46e6b536de73685e5c4cc0960f46ca1fc
Justin Toniazzo <jt@thoughtbot.com>
2024-10-05 09:54:53 -0400

    Add annotations
    Looks like I forgot to check these in

--- spec/models/todo/task_spec.rb
+++ spec/models/todo/task_spec.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: todo_tasks
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  todoist_id :string           not null
+#
 require "rails_helper"
 
 RSpec.describe Todo::Task do
