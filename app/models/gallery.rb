918546ea6013d1e6902603277ee449c6b7861f97
Justin Toniazzo <jutonz@hey.com>
2024-12-06 17:19:08 -0500

    Recent tags v3 (#176)
    Ok once again revisit this

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -34,23 +34,45 @@ class Gallery < ActiveRecord::Base
   def self.hidden = where.not(hidden_at: nil)
 
   def recently_used_tags(excluded_image_ids: nil, image_limit: 4)
-    recently_tagged_image_ids =
-      images
-        .joins(:image_tags)
-        .order(galleries_image_tags: {created_at: :desc})
-        .limit(image_limit)
-        .select(:id)
+    sql = ActiveRecord::Base.sanitize_sql_array([
+      <<~SQL,
+        SELECT DISTINCT
+          galleries_tags.*,
+          galleries_tags.name
+        FROM galleries_tags
+        JOIN galleries_image_tags
+          ON galleries_image_tags.tag_id = galleries_tags.id
+        JOIN (
+          SELECT
+            galleries_images.id AS id,
+            MAX(galleries_image_tags.created_at) AS max_created_at
+          FROM galleries_images
+          JOIN galleries_image_tags
+            ON galleries_image_tags.image_id = galleries_images.id
+          JOIN galleries_tags
+            ON galleries_image_tags.tag_id = galleries_tags.id
+          WHERE galleries_images.gallery_id = ?
+          GROUP BY galleries_images.id
+          ORDER BY max_created_at DESC
+          LIMIT ?
+        ) images
+          ON images.id = galleries_image_tags.image_id
+        ORDER BY galleries_tags.name
+        ;
+      SQL
+      id,
+      image_limit
+    ])
 
-    if excluded_image_ids.present?
-      recently_tagged_image_ids = recently_tagged_image_ids.where.not(id: excluded_image_ids)
+    Galleries::Tag.find_by_sql(sql).then do |result|
+      if excluded_image_ids.present?
+        Galleries::Tag
+          .joins(:images)
+          .where.not(galleries_images: {id: excluded_image_ids})
+          .where(id: result.pluck(:id))
+      else
+        result
+      end
     end
-
-    tag_ids =
-      tags
-        .joins(:images)
-        .where(galleries_images: {id: recently_tagged_image_ids})
-        .distinct
-
-    tags.where(id: tag_ids).order(:name)
   end
 end
