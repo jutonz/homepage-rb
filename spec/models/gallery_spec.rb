c77dcb882cd18d0f681504fbbef2095cf0714e9d
Justin Toniazzo <jutonz@hey.com>
2024-12-13 16:47:22 -0500

    Extract recent tags query into query object (#181)

--- spec/models/gallery_spec.rb
+++ spec/models/gallery_spec.rb
@@ -59,65 +59,19 @@ RSpec.describe Gallery do
   end
 
   describe "#recently_used_tags" do
-    it "returns all tags applied to images tagged recently" do
-      gallery = create(:gallery)
-      image1, image2 = create_pair(:galleries_image, gallery:)
-      tag1, tag2 = create_pair(:galleries_tag, gallery:)
-      image1.add_tag(tag1)
-      image2.add_tag(tag2)
+    it "calls the query object" do
+      allow(Galleries::RecentTagsQuery).to receive(:call)
+      gallery = build(:gallery)
 
-      result = gallery.recently_used_tags.pluck(:id)
+      gallery.recently_used_tags
 
-      expect(result).to eql([tag1.id, tag2.id])
-    end
-
-    it "can limit the number of images referenced" do
-      gallery = create(:gallery)
-      image1, image2 = create_pair(:galleries_image, gallery:)
-      tag1, tag2 = create_pair(:galleries_tag, gallery:)
-      image1.add_tag(tag1)
-      image2.add_tag(tag2)
-
-      result = gallery.recently_used_tags(image_limit: 1).pluck(:id)
-
-      expect(result).to eql([tag2.id])
-    end
-
-    it "can exlude tags for a given image" do
-      gallery = create(:gallery)
-      image1, image2 = create_pair(:galleries_image, gallery:)
-      tag1, tag2 = create_pair(:galleries_tag, gallery:)
-      image1.add_tag(tag1)
-      image2.add_tag(tag2)
-
-      result = gallery.recently_used_tags(excluded_image_ids: [image2.id]).pluck(:id)
-
-      expect(result).to eql([tag1.id])
-    end
-
-    it "doen't include a tag more than once" do
-      gallery = create(:gallery)
-      image1, image2 = create_pair(:galleries_image, gallery:)
-      tag = create(:galleries_tag, gallery:)
-      image1.add_tag(tag)
-      image2.add_tag(tag)
-
-      result = gallery.recently_used_tags.pluck(:id)
-
-      expect(result).to eql([tag.id])
-    end
-
-    it "orders tags by name" do
-      gallery = create(:gallery)
-      image1, image2 = create_pair(:galleries_image, gallery:)
-      tag_b = create(:galleries_tag, gallery:, name: "Tag B")
-      tag_a = create(:galleries_tag, gallery:, name: "Tag A")
-      image1.add_tag(tag_b)
-      image2.add_tag(tag_a)
-
-      result = gallery.recently_used_tags.pluck(:id)
-
-      expect(result).to eql([tag_a.id, tag_b.id])
+      expect(Galleries::RecentTagsQuery)
+        .to have_received(:call)
+        .with(
+          gallery:,
+          excluded_image_ids: nil,
+          image_limit: 10
+        )
     end
   end
 end
