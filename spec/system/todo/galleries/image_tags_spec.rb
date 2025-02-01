ad11990c19c34bfc863c112d9d9b4a6c0283307f
Justin Toniazzo <jt@thoughtbot.com>
2025-02-01 10:50:23 -0500

    Clear tag search field when selecting result
    When you click "Add tag" next to one of the results of a tag search,
    clear the search input. This will make it easier to search for the next
    tag. Note that this just clears the search input. It doesn't remove the
    search results.

--- spec/system/todo/galleries/image_tags_spec.rb
+++ spec/system/todo/galleries/image_tags_spec.rb
@@ -11,9 +11,13 @@ RSpec.describe "Gallery image tags", type: :system do
     visit(gallery_image_path(gallery, image))
 
     click_on("Search")
-    within("turbo-frame#tag-search-result-#{tag.id}") do
+    within("[data-role=tag-search-result]", text: tag.name) do
       click_on("Add tag")
     end
+    expect(page).not_to have_css(
+      "[data-role=tag-search-result]",
+      text: tag.name
+    )
 
     within("turbo-frame#tag_#{tag.id}", text: tag.name) do
       expect(image.reload.tags).to include(tag)
@@ -24,5 +28,29 @@ RSpec.describe "Gallery image tags", type: :system do
 
     expect(page).not_to have_css("turbo-frame#tag_#{tag.id}")
     expect(image.reload.tags).to be_empty
+  end
+
+  it "clears the search input when adding a tag", :js do
+    user = create(:user)
+    gallery = create(:gallery, user:)
+    image = create(:galleries_image, gallery:)
+    tag1 = create(:galleries_tag, gallery:, name: "testing 1")
+    tag2 = create(:galleries_tag, gallery:, name: "testing 2")
+    login_as(user)
+
+    visit(gallery_image_path(gallery, image))
+
+    fill_in("Tag search query", with: "testing")
+    click_on("Search")
+
+    within("[data-role=tag-search-result]", text: tag1.name) do
+      click_on("Add tag")
+    end
+    expect(page).to have_field("Tag search query", with: "")
+
+    expect(page).to have_css(
+      "[data-role=tag-search-result]",
+      text: tag2.name
+    )
   end
 end
