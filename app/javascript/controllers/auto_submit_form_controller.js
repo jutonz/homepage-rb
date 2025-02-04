b001f683ac9bed9588b6a64d7d94bcc442e50b8a
Justin Toniazzo <jt@thoughtbot.com>
2025-02-04 08:51:23 -0500

    Don't autosubmit short queries
    Wait until 4 characters are typed

--- app/javascript/controllers/auto_submit_form_controller.js
+++ app/javascript/controllers/auto_submit_form_controller.js
@@ -2,7 +2,11 @@ import { Controller } from "@hotwired/stimulus"
 import { isProduction } from "../util/rails_env"
 
 export default class extends Controller {
-  submit() {
+  submit(event) {
+    if (event.target.value.length <= 3) {
+      return
+    }
+
     clearTimeout(this.timeout)
     this.timeout = setTimeout(() => {
       this.element.requestSubmit()
