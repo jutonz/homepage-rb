c4fcb8e8a9f4ba7386c2abb4aa4ef4cbb7b7086c
Justin Toniazzo <jutonz@hey.com>
2025-02-21 20:41:18 -0500

    Upgrade pinned activestorage js (#251)
    I should figure out how to make this work with dependabot

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,5 +7,5 @@ pin_all_from "app/javascript/util", under: "util"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin "@rails/activestorage", to: "@rails--activestorage.js" # @7.2.200
+pin "@rails/activestorage", to: "@rails--activestorage.js" # @8.0.100
 pin "just-debounce" # @1.1.0
