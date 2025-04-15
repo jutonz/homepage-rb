906c211a290b907e5e6752fefdd8cab859a20955
Justin Toniazzo <jutonz@hey.com>
2025-04-14 21:17:45 -0400

    setup prometheus exporter (#276)

---
+++ config/initializers/prometheus_exporter.rb
@@ -0,0 +1,9 @@
+if Rails.application.config.x.enable_metrics
+  require "prometheus_exporter/middleware"
+
+  # This reports stats per request like HTTP status and timings
+  Rails.application.middleware.unshift(
+    PrometheusExporter::Middleware,
+    instrument: :prepend
+  )
+end
