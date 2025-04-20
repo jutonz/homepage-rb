bb96cc56c4b443fdce5f3da7760d3df3e9e24b26
Justin Toniazzo <jt@thoughtbot.com>
2025-04-20 10:07:00 -0400

    Cache similar images

--- app/models/galleries/similar_images.rb
+++ app/models/galleries/similar_images.rb
@@ -17,22 +17,39 @@ module Galleries
     attr_reader :image
 
     def similar_images
-      cluster =
-        Galleries::Image
-          .joins(:tags)
-          .where(tags: {id: image.tag_ids})
-          .distinct
-          .where.not(id: image.id)
+      ids = similar_image_ids
+      return Galleries::Image.none if ids.empty?
 
-      this_weighted_tags = weighted_tags(image)
+      distinct_image_ids = Galleries::Image.distinct.where(id: ids)
+      Galleries::Image
+        .includes(:gallery)
+        .with_attached_file
+        .where(id: distinct_image_ids)
+        .order(Arel.sql("array_position(ARRAY[?], id)", ids))
+    end
 
-      cluster.sort_by do |other|
-        other_weighted_tags = weighted_tags(other)
+    def similar_image_ids
+      Rails.cache.fetch(
+        "similar-image-ids-#{image.id}",
+        expires_in: 1.hour
+      ) do
+        cluster =
+          Galleries::Image
+            .distinct
+            .joins(:tags)
+            .where(tags: {id: image.tag_ids})
+            .where.not(id: image.id)
 
-        (this_weighted_tags.keys & other_weighted_tags.keys).sum do |tag|
-          this_weighted_tags[tag] * other_weighted_tags[tag]
-        end
-      end.reverse
+        this_weighted_tags = weighted_tags(image)
+
+        cluster.sort_by do |other|
+          other_weighted_tags = weighted_tags(other)
+
+          (this_weighted_tags.keys & other_weighted_tags.keys).sum do |tag|
+            this_weighted_tags[tag] * other_weighted_tags[tag]
+          end
+        end.reverse.map(&:id)
+      end
     end
 
     def weighted_tags(image)
