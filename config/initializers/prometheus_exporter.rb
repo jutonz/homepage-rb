775a95b6db29daabf21ab881196d51cebc4ce0c2
Justin Toniazzo <jt@thoughtbot.com>
2025-04-23 10:27:38 -0400

    fix docker build

--- config/initializers/prometheus_exporter.rb
+++ config/initializers/prometheus_exporter.rb
@@ -1,4 +1,5 @@
 if Rails.application.config.x.enable_metrics
+  require "prometheus_exporter"
   require "prometheus_exporter/middleware"
 
   # This reports stats per request like HTTP status and timings
