a548b981d1e50caba48a815560106d10e666f514
Justin Toniazzo <jutonz@hey.com>
2025-06-25 10:00:39 -0400

    Convert image tags partial to component (#330)
    These partials are pretty unweildy, so I'm converting many of them to
    view components.

---
+++ app/components/galleries/image_tags_component.rb
@@ -0,0 +1,45 @@
+module Galleries
+  class ImageTagsComponent < ViewComponent::Base
+    include Rails.application.routes.url_helpers
+    include Turbo::FramesHelper
+
+    erb_template <<~ERB
+      <%= turbo_frame_tag("image-tags") do %>
+        <div class="flex mb-5 flex-col gap-3" data-role="tags">
+          <span class="hidden only:block">
+            This image doesn't have any tags yet.
+          </span>
+          <% @tags.each do |tag| %>
+            <%= turbo_frame_tag(tag, data: {role: "tag"}) do %>
+              <div class="flex gap-3 items-center">
+                <div>
+                  <%= link_to(
+                    tag.display_name,
+                    gallery_tag_path(@gallery, tag),
+                    data: {
+                      turbo: false,
+                      role: "tag-link"
+                    }
+                  ) %>
+                </div>
+                <%= button_to(
+                  "Remove",
+                  gallery_image_tag_path(@gallery, @image, tag),
+                  method: :delete,
+                  class: "button button--danger",
+                  data: {turbo_confirm: "Are you sure?"}
+                ) %>
+              </div>
+            <% end %>
+          <% end %>
+        </div>
+      <% end %>
+    ERB
+
+    def initialize(image:)
+      @image = image
+      @gallery = image.gallery
+      @tags = image.tags.order(:name)
+    end
+  end
+end
---
+++ spec/components/galleries/image_tags_component_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+
+RSpec.describe Galleries::ImageTagsComponent, type: :component do
+  include Rails.application.routes.url_helpers
+
+  it "renders tags ordered by name" do
+    gallery = create(:gallery)
+    image = create(:galleries_image, gallery:)
+    tag_b = create(:galleries_tag, gallery:, name: "Tag B")
+    tag_a = create(:galleries_tag, gallery:, name: "Tag A")
+    [tag_a, tag_b].each { image.add_tag(it) }
+    component = described_class.new(image:)
+    render_inline(component)
+
+    expect(page).to have_css("[data-role=tags]")
+    tags = page.all("[data-role=tag]")
+    expect(tags.first.text).to include(tag_a.display_name)
+    expect(tags.last.text).to include(tag_b.display_name)
+  end
+
+  it "renders a link to the tag" do
+    gallery = create(:gallery)
+    image = create(:galleries_image, gallery:)
+    tag = create(:galleries_tag, gallery:)
+    image.add_tag(tag)
+    component = described_class.new(image:)
+    render_inline(component)
+
+    expect(page).to have_link(
+      tag.display_name,
+      href: gallery_tag_path(gallery, tag)
+    )
+  end
+end
