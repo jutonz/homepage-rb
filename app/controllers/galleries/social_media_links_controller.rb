f76bdea7c8368369fbb140f78201d1f1a7b3d545
Justin Toniazzo <jutonz@hey.com>
2025-06-25 14:55:51 -0400

    Allow creating and editing social media links (#338)

---
+++ app/controllers/galleries/social_media_links_controller.rb
@@ -0,0 +1,67 @@
+module Galleries
+  class SocialMediaLinksController < ApplicationController
+    before_action :ensure_authenticated!
+
+    def new
+      @gallery = find_gallery
+      @tag = find_tag(@gallery)
+      @social_media_link = @tag.social_media_links.build
+      render :new
+    end
+
+    def create
+      @gallery = find_gallery
+      @tag = find_tag(@gallery)
+      @social_media_link = @tag.social_media_links.build(link_params)
+
+      if @social_media_link.save
+        redirect_to(
+          gallery_tag_path(@gallery, @tag),
+          notice: "Created social media link!"
+        )
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+    def edit
+      @gallery = find_gallery
+      @tag = find_tag(@gallery)
+      @social_media_link = find_link(@tag)
+      render :edit
+    end
+
+    def update
+      @gallery = find_gallery
+      @tag = find_tag(@gallery)
+      @social_media_link = find_link(@tag)
+
+      if @social_media_link.update(link_params)
+        redirect_to(
+          gallery_tag_path(@gallery, @tag),
+          notice: "Updated social media link!"
+        )
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def find_gallery
+      current_user.galleries.find(params[:gallery_id])
+    end
+
+    def find_tag(gallery)
+      gallery.tags.find(params[:tag_id])
+    end
+
+    def find_link(tag)
+      tag.social_media_links.find(params[:id])
+    end
+
+    def link_params
+      params.expect(social_media_link: %i[platform username])
+    end
+  end
+end
