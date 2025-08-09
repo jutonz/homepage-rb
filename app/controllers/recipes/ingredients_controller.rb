28c6e92858b9bf957a57a46d577395eb18974132
Justin Toniazzo <jt@thoughtbot.com>
2025-08-09 14:49:08 -0400

    extract

--- app/controllers/recipes/ingredients_controller.rb
+++ app/controllers/recipes/ingredients_controller.rb
@@ -5,13 +5,13 @@ module Recipes
     def index
       @recipe = find_recipe
       @recipe_ingredients = @recipe.recipe_ingredients.includes(:ingredient)
-      @available_ingredients = current_user.recipes_ingredients.order(:name)
+      @available_ingredients = find_available_ingredients
     end
 
     def new
       @recipe = find_recipe
       @recipe_ingredient = @recipe.recipe_ingredients.build
-      @available_ingredients = current_user.recipes_ingredients.order(:name)
+      @available_ingredients = find_available_ingredients
     end
 
     def create
@@ -21,7 +21,7 @@ module Recipes
       if @recipe_ingredient.save
         redirect_to recipe_ingredients_path(@recipe), notice: "Ingredient was successfully added to recipe."
       else
-        @available_ingredients = current_user.recipes_ingredients.order(:name)
+        @available_ingredients = find_available_ingredients
         render :new, status: :unprocessable_content
       end
     end
@@ -29,7 +29,7 @@ module Recipes
     def edit
       @recipe = find_recipe
       @recipe_ingredient = find_recipe_ingredient
-      @available_ingredients = current_user.recipes_ingredients.order(:name)
+      @available_ingredients = find_available_ingredients
     end
 
     def update
@@ -39,11 +39,7 @@ module Recipes
       if @recipe_ingredient.update(recipe_ingredient_params)
         redirect_to recipe_ingredients_path(@recipe), notice: "Recipe ingredient was successfully updated."
       else
-        @available_ingredients = current_user.recipes_ingredients.order(:name)
-        # Reload the ingredient association since it might be nil after failed validation
-        if @recipe_ingredient.ingredient_id.present?
-          @recipe_ingredient.ingredient = Recipes::Ingredient.find(@recipe_ingredient.ingredient_id)
-        end
+        @available_ingredients = find_available_ingredients
         render :edit, status: :unprocessable_content
       end
     end
@@ -63,6 +59,10 @@ module Recipes
 
     def find_recipe_ingredient
       @recipe.recipe_ingredients.find(params[:id])
+    end
+
+    def find_available_ingredients
+      current_user.recipes_ingredients.order(:name)
     end
 
     def recipe_ingredient_params
