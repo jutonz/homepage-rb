6d54b5c8b9e33a841a02570dcb640bcee73af519
Justin Toniazzo <jt@thoughtbot.com>
2025-08-09 20:47:38 -0400

    tests

--- spec/models/recipes/recipe_spec.rb
+++ spec/models/recipes/recipe_spec.rb
@@ -23,8 +23,9 @@ RSpec.describe Recipes::Recipe, type: :model do
   subject { build(:recipes_recipe) }
 
   it { is_expected.to belong_to(:user) }
-  it { is_expected.to have_many(:recipe_ingredients).dependent(:destroy) }
   it { is_expected.to have_many(:ingredients).through(:recipe_ingredients) }
+  it { is_expected.to have_many(:recipe_ingredients).dependent(:destroy) }
+  it { is_expected.to have_rich_text(:instructions) }
 
   it { is_expected.to validate_presence_of(:name) }
 
