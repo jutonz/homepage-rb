cc0d77c9c46a31a4d943f7c21f2396b82bbf3bcd
Justin Toniazzo <jt@thoughtbot.com>
2025-08-13 11:49:32 -0400

    refactor policy spec

--- spec/policies/user_group_invitation_policy_spec.rb
+++ spec/policies/user_group_invitation_policy_spec.rb
@@ -1,55 +1,48 @@
 require "rails_helper"
 
 RSpec.describe UserGroupInvitationPolicy, type: :policy do
-  subject { UserGroupInvitationPolicy }
-
   permissions :create? do
     it "allows group owners to create invitations" do
-      owner = create(:user)
-      user_group = create(:user_group, owner:)
-      invitation = UserGroupInvitation.new(user_group:)
+      owner = build(:user)
+      user_group = build(:user_group, owner:)
+      invitation = build(:user_group_invitation, user_group:)
 
-      expect(subject).to permit(owner, invitation)
+      expect(described_class).to permit(owner, invitation)
     end
 
     it "denies non-owners from creating invitations" do
-      owner = create(:user)
-      non_owner = create(:user)
-      user_group = create(:user_group, owner:)
-      invitation = UserGroupInvitation.new(user_group:)
+      owner, non_owner = build_pair(:user)
+      user_group = build(:user_group, owner:)
+      invitation = build(:user_group_invitation, user_group:)
 
-      expect(subject).not_to permit(non_owner, invitation)
+      expect(described_class).not_to permit(non_owner, invitation)
     end
 
     it "denies anonymous users from creating invitations" do
-      user_group = create(:user_group)
-      invitation = UserGroupInvitation.new(user_group:)
-
-      expect(subject).not_to permit(nil, invitation)
+      invitation = build(:user_group_invitation)
+      expect(described_class).not_to permit(nil, invitation)
     end
   end
 
   permissions :show? do
     it "allows anyone to view invitations" do
-      invitation = create(:user_group_invitation)
+      invitation = build(:user_group_invitation)
 
-      expect(subject).to permit(nil, invitation)
-      expect(subject).to permit(create(:user), invitation)
+      expect(described_class).to permit(nil, invitation)
+      expect(described_class).to permit(build(:user), invitation)
     end
   end
 
-  describe "Scope" do
+  describe described_class::Scope do
     it "returns invitations for groups owned by user" do
-      owner = create(:user)
-      other_user = create(:user)
-
+      owner, other_user = create_pair(:user)
       owned_group = create(:user_group, owner:)
       other_group = create(:user_group, owner: other_user)
 
       owned_invitation = create(:user_group_invitation, user_group: owned_group)
       create(:user_group_invitation, user_group: other_group)
 
-      scope = UserGroupInvitationPolicy::Scope.new(owner, UserGroupInvitation).resolve
+      scope = described_class.new(owner, UserGroupInvitation).resolve
 
       expect(scope).to contain_exactly(owned_invitation)
     end
@@ -57,7 +50,7 @@ RSpec.describe UserGroupInvitationPolicy, type: :policy do
     it "returns empty scope for anonymous users" do
       create(:user_group_invitation)
 
-      scope = UserGroupInvitationPolicy::Scope.new(nil, UserGroupInvitation).resolve
+      scope = described_class.new(nil, UserGroupInvitation).resolve
 
       expect(scope).to be_empty
     end
