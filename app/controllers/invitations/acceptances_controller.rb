d693e0c32a204b7797c4b03ed45d0caace6a34d0
Justin Toniazzo <jt@thoughtbot.com>
2025-08-13 12:00:54 -0400

    specs

--- app/controllers/invitations/acceptances_controller.rb
+++ app/controllers/invitations/acceptances_controller.rb
@@ -8,13 +8,25 @@ module Invitations
       authorize(@invitation, :show?)
 
       if @invitation.accept!
-        redirect_to @invitation.user_group, notice: "Welcome to #{@invitation.user_group.name}!"
+        redirect_to(
+          user_group_path(@invitation.user_group),
+          notice: "Welcome to #{@invitation.user_group.name}!"
+        )
       elsif @invitation.expired?
-        redirect_to invitation_path(@invitation.token), alert: "This invitation has expired."
+        redirect_to(
+          invitation_path(@invitation.token),
+          alert: "This invitation has expired."
+        )
       elsif @invitation.accepted?
-        redirect_to @invitation.user_group, notice: "You're already a member of this group."
+        redirect_to(
+          user_group_path(@invitation.user_group),
+          notice: "You're already a member of this group."
+        )
       else
-        redirect_to invitation_path(@invitation.token), alert: "Unable to accept invitation. Please try again."
+        redirect_to(
+          invitation_path(@invitation.token),
+          alert: "Unable to accept invitation. Please try again."
+        )
       end
     end
   end
--- spec/requests/invitations/acceptances_spec.rb
+++ spec/requests/invitations/acceptances_spec.rb
@@ -1,76 +1,64 @@
 require "rails_helper"
 
 RSpec.describe Invitations::AcceptancesController, type: :request do
-  describe "POST /invitations/:invitation_token/acceptance" do
+  describe "create" do
     it "accepts valid invitation when user exists" do
       invitation = create(:user_group_invitation)
       user = create(:user, email: invitation.email)
-
       login_as(user)
 
-      post invitation_acceptance_path(invitation.token)
+      post(invitation_acceptance_path(invitation.token))
 
+      expect(response).to redirect_to(
+        user_group_path(invitation.user_group)
+      )
       expect(invitation.reload.accepted_at).to be_present
-      expect(response).to redirect_to(invitation.user_group)
       expect(flash[:notice]).to include("Welcome to #{invitation.user_group.name}")
-    end
-
-    it "creates user group membership on acceptance" do
-      invitation = create(:user_group_invitation)
-      user = create(:user, email: invitation.email)
-
-      login_as(user)
-
-      expect {
-        post invitation_acceptance_path(invitation.token)
-      }.to change { invitation.user_group.user_group_memberships.count }.by(1)
-
       membership = invitation.user_group.user_group_memberships.last
       expect(membership.user).to eq(user)
     end
 
     it "handles expired invitations" do
-      invitation = create(:user_group_invitation, expires_at: 1.day.ago)
+      invitation = create(:user_group_invitation, :expired)
       user = create(:user, email: invitation.email)
-
       login_as(user)
 
-      post invitation_acceptance_path(invitation.token)
+      post(invitation_acceptance_path(invitation.token))
 
-      expect(invitation.reload.accepted_at).to be_nil
       expect(response).to redirect_to(invitation_path(invitation.token))
+      expect(invitation.reload.accepted_at).to be_nil
       expect(flash[:alert]).to include("expired")
     end
 
     it "handles already accepted invitations" do
       invitation = create(:user_group_invitation, :accepted)
       user = create(:user, email: invitation.email)
-
       login_as(user)
 
-      post invitation_acceptance_path(invitation.token)
+      post(invitation_acceptance_path(invitation.token))
 
-      expect(response).to redirect_to(invitation.user_group)
+      expect(response).to redirect_to(
+        user_group_path(invitation.user_group)
+      )
       expect(flash[:notice]).to include("already a member")
     end
 
     it "handles non-matching user email" do
       invitation = create(:user_group_invitation)
       different_user = create(:user, email: "different@example.com")
-
       login_as(different_user)
 
-      post invitation_acceptance_path(invitation.token)
+      post(invitation_acceptance_path(invitation.token))
 
-      expect(invitation.reload.accepted_at).to be_nil
       expect(response).to redirect_to(invitation_path(invitation.token))
+      expect(invitation.reload.accepted_at).to be_nil
       expect(flash[:alert]).to include("Unable to accept invitation")
     end
 
     it "requires authentication" do
       invitation = create(:user_group_invitation)
 
-      post invitation_acceptance_path(invitation.token)
+      post(invitation_acceptance_path(invitation.token))
 
       expect(response).to redirect_to(new_session_path)
     end
@@ -79,7 +67,7 @@ RSpec.describe Invitations::AcceptancesController, type: :request do
       user = create(:user)
       login_as(user)
 
-      post invitation_acceptance_path("invalid-token")
+      post(invitation_acceptance_path("invalid-token"))
 
       expect(response).to have_http_status(:not_found)
     end
