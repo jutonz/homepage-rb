ddcae34c6a7545f74410048fa61fcf9c1c1183ce
Justin Toniazzo <jt@thoughtbot.com>
2025-08-13 13:16:58 -0400

    remove some tetss

--- spec/requests/invitations_spec.rb
+++ spec/requests/invitations_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe InvitationsController, type: :request do
-  describe "GET /invitations/:token" do
+  describe "show" do
     it "shows active invitation successfully" do
       owner = create(:user, email: "owner@example.com")
       user_group = create(:user_group, owner:, name: "Test Group", users_count: 5)
@@ -68,117 +68,6 @@ RSpec.describe InvitationsController, type: :request do
       get invitation_path("99999")
 
       expect(response).to have_http_status(:not_found)
-    end
-
-    it "shows invitation for recently created invitation" do
-      invitation = create(:user_group_invitation)
-
-      get invitation_path(invitation.token)
-
-      expect(response).to be_successful
-      expect(response.body).to include("You're Invited!")
-    end
-
-    it "handles invitation close to expiration" do
-      owner = create(:user)
-      user_group = create(:user_group, owner:)
-      invitation = create(:user_group_invitation,
-        user_group:,
-        invited_by: owner,
-        expires_at: 1.hour.from_now)
-
-      get invitation_path(invitation.token)
-
-      expect(response).to be_successful
-      expect(response.body).to include("You're Invited!")
-      expect(response.body).to include("Accept Invitation")
-      expect(response.body).not_to include("Expired")
-    end
-
-    it "shows correct page title for each state" do
-      owner = create(:user)
-      user_group = create(:user_group, owner:)
-
-      # Active invitation
-      active_invitation = create(:user_group_invitation,
-        user_group:,
-        invited_by: owner,
-        expires_at: 3.days.from_now,
-        accepted_at: nil)
-      get invitation_path(active_invitation.token)
-      expect(response.body).to include("Group Invitation")
-
-      # Expired invitation
-      expired_invitation = create(:user_group_invitation, :expired,
-        user_group:,
-        invited_by: owner)
-      get invitation_path(expired_invitation.token)
-      expect(response.body).to include("Invitation Expired")
-
-      # Accepted invitation
-      accepted_invitation = create(:user_group_invitation, :accepted,
-        user_group:,
-        invited_by: owner)
-      get invitation_path(accepted_invitation.token)
-      expect(response.body).to include("Already a Member")
-    end
-
-    it "includes proper navigation breadcrumbs" do
-      invitation = create(:user_group_invitation)
-
-      get invitation_path(invitation.token)
-
-      expect(response).to be_successful
-      expect(response.body).to include("Home")
-      expect(response.body).to include(home_path)
-    end
-
-    it "shows invitation with special characters in group name" do
-      owner = create(:user)
-      user_group = create(:user_group, owner:, name: "Test & Development Group")
-      invitation = create(:user_group_invitation, user_group:, invited_by: owner)
-
-      get invitation_path(invitation.token)
-
-      expect(response).to be_successful
-      expect(response.body).to include("Test &amp; Development Group")
-    end
-
-    it "shows invitation with long group name" do
-      owner = create(:user)
-      long_name = "A" * 100
-      user_group = create(:user_group, owner:, name: long_name)
-      invitation = create(:user_group_invitation, user_group:, invited_by: owner)
-
-      get invitation_path(invitation.token)
-
-      expect(response).to be_successful
-      expect(response.body).to include(long_name)
-    end
-
-    it "includes correct HTTP headers" do
-      invitation = create(:user_group_invitation)
-
-      get invitation_path(invitation.token)
-
-      expect(response).to be_successful
-      expect(response.headers["Content-Type"]).to include("text/html")
-    end
-
-    it "handles invitation with zero member count" do
-      owner = create(:user)
-      user_group = create(:user_group, owner:, users_count: 0)
-      invitation = create(:user_group_invitation,
-        user_group:,
-        invited_by: owner,
-        expires_at: 3.days.from_now,
-        accepted_at: nil)
-
-      get invitation_path(invitation.token)
-
-      expect(response).to be_successful
-      expect(response.body).to include("Current Members:")
-      expect(response.body).to include("0")
     end
   end
 end
