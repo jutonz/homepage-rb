ee3ca56975c9233a730005cbd288e2cbc755bef9
Justin Toniazzo <jt@thoughtbot.com>
2025-08-13 13:09:00 -0400

    combine views

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -4,15 +4,5 @@ class InvitationsController < ApplicationController
   def show
     @invitation = UserGroupInvitation.find_by!(token: params[:token])
     authorize(@invitation, :show?)
-
-    if @invitation.expired?
-      render :expired and return
-    end
-
-    if @invitation.accepted?
-      render :already_accepted and return
-    end
-
-    # Show the invitation details for acceptance
   end
 end
