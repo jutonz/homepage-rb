5e5a1bcb4858abc391b96227f0ac08199b6ff93a
Justin Toniazzo <jt@thoughtbot.com>
2025-08-13 10:09:58 -0400

    refactor

--- app/mailers/user_group_invitation_mailer.rb
+++ app/mailers/user_group_invitation_mailer.rb
@@ -1,13 +1,11 @@
 class UserGroupInvitationMailer < ApplicationMailer
   def invitation(user_group_invitation)
     @invitation = user_group_invitation
-    @user_group = user_group_invitation.user_group
-    @invited_by = user_group_invitation.invited_by
     @accept_url = invitation_accept_url(token: user_group_invitation.token)
 
     mail(
       to: user_group_invitation.email,
-      subject: "You're invited to join #{@user_group.name}"
+      subject: "You're invited to join #{user_group_invitation.user_group.name}"
     )
   end
 
