3922003bb458dfad0c8d9ad478e33ab5be7b57ec
Justin Toniazzo <jutonz@hey.com>
2025-10-25 12:12:16 -0400

    Update flash styles (#511)

--- app/components/flash_component.rb
+++ app/components/flash_component.rb
@@ -2,7 +2,7 @@ class FlashComponent < ApplicationComponent
   erb_template <<~ERB
     <%= tag.div(
       @message,
-      class: "border-cyan-100 border-4 rounded-sm px-4 py-3 shadow-md min-w-[300px]",
+      class: classes,
       data: {
         role: "flash",
         flash_type: @type
@@ -13,5 +13,26 @@ class FlashComponent < ApplicationComponent
   def initialize(message:, type:)
     @message = message
     @type = type
+  end
+
+  private
+
+  def classes
+    base_classes =
+      "border-4 rounded-sm px-4 py-3 shadow-md md:min-w-[300px]"
+    "#{base_classes} border-#{color_name}-500 bg-#{color_name}-50"
+  end
+
+  def color_name
+    case @type.to_sym
+    when :success
+      "emerald"
+    when :error, :alert
+      "rose"
+    when :warning
+      "amber"
+    else # :notice and other types
+      "sky"
+    end
   end
 end
