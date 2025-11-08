3e542e8a4416b9043b37110fa87bd73c02d35260
Justin Toniazzo <jutonz@hey.com>
2025-11-08 10:56:36 -0500

    use new activestore js bundle (#531)
    i missed upgrading this when upgrading to rails 8.1

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,7 +7,7 @@ pin_all_from "app/javascript/util", under: "util"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin "@rails/activestorage", to: "@rails--activestorage.js" # @8.0.201
+pin "@rails/activestorage", to: "@rails--activestorage.js" # @8.1.100
 pin "just-debounce" # @1.1.0
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.esm.js"
