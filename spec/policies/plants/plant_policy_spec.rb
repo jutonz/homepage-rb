a223264cbea5f47749eb82f26c9a5f938e7bd669
Justin Toniazzo <jt@thoughtbot.com>
2026-02-07 11:56:46 -0500

    Add plant policy spec

---
+++ spec/policies/plants/plant_policy_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe(Plants::PlantPolicy) do
+  describe("inheritance") do
+    it "inherits from UserOwnedPolicy" do
+      expect(described_class.superclass).to eq(UserOwnedPolicy)
+    end
+  end
+end
