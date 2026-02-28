b5a757fed232137cdb660a870362c40fb1981fe5
Justin Toniazzo <jt@thoughtbot.com>
2026-02-28 10:50:16 -0500

    Preserve gallery query params when toggling select mode
    Use a select-mode Stimulus controller to build Select/Cancel navigation from the current URL, preserving pagination and filters after Turbo navigation. Keep links disabled in HTML until the controller connects, then enable them.

---
+++ app/javascript/controllers/select_mode_controller.js
@@ -0,0 +1,35 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect() {
+    this.element.removeAttribute("disabled")
+  }
+
+  enter(event) {
+    event.preventDefault()
+
+    const url = new URL(window.location.href)
+    url.searchParams.set("select", "true")
+
+    this.visit(url)
+  }
+
+  exit(event) {
+    event.preventDefault()
+
+    const url = new URL(window.location.href)
+    url.searchParams.delete("select")
+    url.searchParams.delete("selected_ids[]")
+
+    this.visit(url)
+  }
+
+  visit(url) {
+    if (window.Turbo) {
+      window.Turbo.visit(url.toString())
+      return
+    }
+
+    window.location.assign(url.toString())
+  }
+}
