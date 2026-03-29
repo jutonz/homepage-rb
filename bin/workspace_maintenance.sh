3ffc6af07d40fb0b3c1caad3d3dbb98caf4fcaa0
Justin Toniazzo <jutonz@hey.com>
2026-03-29 19:14:37 -0400

    Update workspace maintenance precompile step

--- bin/workspace_maintenance.sh
+++ bin/workspace_maintenance.sh
@@ -4,8 +4,6 @@ set -euo pipefail
 
 export DEBIAN_FRONTEND=noninteractive
 
-npm i
-npm exec --no playwright install --with-deps
-
 bundle install
 RAILS_ENV=test bin/rails db:prepare
+RAILS_ENV=test mise exec -- bin/rails assets:precompile
