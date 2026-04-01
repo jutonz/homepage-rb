f7431a737237f241bf7d96de5116c1efdb0f9fe0
Justin Toniazzo <jt@thoughtbot.com>
2026-04-01 10:21:15 -0400

    Preserve query params through bulk tag redirects
    Use request.query_parameters to generically forward all existing
    query params (page, tag_ids, etc.) instead of manually passing
    individual params. Removes the hardcoded page hidden field from
    the bulk tag form.

--- app/controllers/galleries/bulk_tags_controller.rb
+++ app/controllers/galleries/bulk_tags_controller.rb
@@ -9,24 +9,22 @@ module Galleries
         Galleries::BulkTag.new(bulk_tag_params.merge(gallery: @gallery))
       )
 
+      redirect_params = existing_query_params
+      ids = Array(@bulk_tag.image_ids).compact_blank
+      if ids.present?
+        redirect_params = redirect_params.merge(
+          "selected_ids" => ids
+        )
+      end
+
       if @bulk_tag.save
         redirect_to(
-          gallery_path(
-            @gallery,
-            select: true,
-            selected_ids: @bulk_tag.image_ids,
-            page: params[:page].presence
-          ),
+          gallery_path(@gallery, **redirect_params),
           notice: "Tag added to selected images"
         )
       else
         redirect_to(
-          gallery_path(
-            @gallery,
-            select: true,
-            selected_ids: @bulk_tag.image_ids,
-            page: params[:page].presence
-          ),
+          gallery_path(@gallery, **redirect_params),
           alert: "Could not add tag"
         )
       end
@@ -39,7 +37,12 @@ module Galleries
     end
 
     def bulk_tag_params
-      params.require(:bulk_tag).permit(:tag_id, image_ids: [])
+      params.require(:bulk_tag)
+        .permit(:tag_id, image_ids: [])
+    end
+
+    def existing_query_params
+      request.query_parameters
     end
   end
 end
