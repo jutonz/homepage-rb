a495cc5b1baaddf70aa936dece03ebc673dc4aa9
Justin Toniazzo <jt@thoughtbot.com>
2026-04-03 15:05:47 -0400

    Add system test for processing images broadcast
    Verifies that images are removed from the processing
    images page when ImageProcessingJob completes. Changes
    test cable adapter from test to async so Turbo Stream
    broadcasts reach the browser in system tests.

---
+++ spec/system/galleries/processing_images_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+RSpec.describe "Gallery processing images",
+  type: :system do
+  it "removes images when processing completes",
+    :js do
+    user = create(:user)
+    gallery = create(:gallery, user:)
+    image = create(
+      :galleries_image, :with_real_file, gallery:
+    )
+    login_as(user)
+
+    visit(
+      gallery_processing_images_path(gallery)
+    )
+
+    expect(page).to have_css(
+      "#processing_image_#{image.id}"
+    )
+
+    Galleries::ImageProcessingJob
+      .perform_now(image)
+
+    expect(page).not_to have_css(
+      "#processing_image_#{image.id}"
+    )
+  end
+end
