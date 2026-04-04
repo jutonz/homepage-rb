423d7bd9d456532e4975dd18a7613d603f39bc7d
Justin Toniazzo <jt@thoughtbot.com>
2026-04-04 09:15:14 -0400

    Add Stimulus controller for processing images reconciliation
    Subscribes to a custom ActionCable channel that periodically syncs
    unprocessed image IDs and removes stale DOM elements when Turbo
    Stream broadcasts are missed.

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,6 +7,7 @@ pin_all_from "app/javascript/util", under: "util"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "@rails/actioncable", to: "actioncable.esm.js"
 pin "@rails/activestorage", to: "@rails--activestorage.js" # @8.1.100
 pin "just-debounce" # @1.1.0
 pin "trix"
