f797b9a078fccc0545ef278d8cf601e6a2862fb3
Justin Toniazzo <jt@thoughtbot.com>
2026-04-18 10:47:08 -0400

    Add failing system spec for Enter-to-add filter on gallery page

--- spec/system/galleries/filtering_spec.rb
+++ spec/system/galleries/filtering_spec.rb
@@ -37,4 +37,35 @@ RSpec.describe "Gallery filtering" do
     ).click
     expect(page).to have_css("[data-image-id='#{image.id}']")
   end
+
+  it "adds a filter when pressing Enter on a search result", :js do
+    user = create(:user)
+    gallery = create(:gallery, user:)
+    image = create(:galleries_image, :with_real_file, gallery:)
+    tag = create(:galleries_tag, gallery:, name: "alpha")
+    create(:galleries_tag, gallery:, name: "alpine")
+    image.add_tag(tag)
+    login_as(user)
+
+    visit(gallery_path(gallery))
+
+    fill_in("Tag search query", with: "alp")
+    click_on("Search")
+    expect(page).to have_css(
+      "[data-role=tag-search-result]",
+      text: tag.name
+    )
+
+    find_field("Tag search query").send_keys(:enter)
+
+    expect(page).to have_css(
+      "[data-role=tag-filter-remove-button]",
+      text: tag.name
+    )
+    expect(page).to have_css("[data-image-id='#{image.id}']")
+    expect(page).to have_field("Tag search query", with: "alp")
+    expect(page).to have_selector(
+      "[aria-label='Tag search query']:focus"
+    )
+  end
 end
