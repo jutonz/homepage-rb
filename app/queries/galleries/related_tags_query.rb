773f16288fa391d7b5808cef73cf9d82c1a6140d
Justin Toniazzo <jt@thoughtbot.com>
2026-05-25 14:29:23 -0400

    Add exclude_tag_ids and limit options to RelatedTagsQuery

--- app/queries/galleries/related_tags_query.rb
+++ app/queries/galleries/related_tags_query.rb
@@ -4,26 +4,28 @@ module Galleries
 
     def self.call(...) = new(...).call
 
-    def initialize(tag:)
+    def initialize(tag:, exclude_tag_ids: [], limit: 10)
       @tag = tag
+      @exclude_tag_ids = exclude_tag_ids
+      @limit = limit
     end
 
     def call
       Galleries::Tag
         .where(gallery_id: tag.gallery_id)
-        .where.not(id: tag.id)
+        .where.not(id: [tag.id, *exclude_tag_ids])
         .joins(:image_tags)
         .where(galleries_image_tags: {image_id: tag.image_ids})
         .group("galleries_tags.id")
         .select("galleries_tags.*, COUNT(*) AS shared_count")
         .order(Arel.sql("COUNT(*) DESC"))
-        .limit(10)
+        .limit(limit)
         .includes(:gallery)
         .map { Result.new(tag: it, shared_count: it.shared_count.to_i) }
     end
 
     private
 
-    attr_reader :tag
+    attr_reader :tag, :exclude_tag_ids, :limit
   end
 end
