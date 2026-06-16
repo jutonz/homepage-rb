cf13e728ce805ac3664fd05edee995aa66850d71
Justin Toniazzo <jt@thoughtbot.com>
2026-06-16 09:24:38 -0400

    Combine key-image upload tests
    Merge the single-file and multi-file key_image specs into one test
    that covers both: uploads many files, asserts the first becomes the
    key image.

--- spec/models/plants/plant_image_upload_spec.rb
+++ spec/models/plants/plant_image_upload_spec.rb
@@ -27,23 +27,7 @@ RSpec.describe Plants::PlantImageUpload do
       expect(result.saved?).to(eq(true))
     end
 
-    it "sets the new image as the key image when none is set" do
-      user = create(:user)
-      plant = create(:plant, user:)
-      files = [fixture_file_upload("audiosurf.jpg", "image/jpeg")]
-
-      service = described_class.new(
-        plant:,
-        files:,
-        taken_at: "2024-01-02"
-      )
-
-      service.save
-
-      expect(plant.reload.key_image).to(eq(plant.plant_images.first))
-    end
-
-    it "sets the first image as the key image when uploading many" do
+    it "sets the first new image as the key image when none is set" do
       user = create(:user)
       plant = create(:plant, user:)
       files = [
