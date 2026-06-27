4ca77e3915982f6ef5dc9a44ea9600e690a99eda
Justin Toniazzo <jt@thoughtbot.com>
2026-06-27 14:33:17 -0400

    Add Galleries::RemoteVideoDownloadPolicy

---
+++ app/policies/galleries/remote_video_download_policy.rb
@@ -0,0 +1,23 @@
+module Galleries
+  class RemoteVideoDownloadPolicy < ApplicationPolicy
+    def index?
+      user_owns_gallery?
+    end
+
+    def new?
+      user_owns_gallery?
+    end
+
+    def create?
+      user_owns_gallery?
+    end
+
+    private
+
+    def user_owns_gallery?
+      return false unless record.gallery
+
+      user && record.gallery.user == user
+    end
+  end
+end
