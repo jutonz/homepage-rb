13df8de2ddcbf4a6c458fcab84671b020ca041c1
Justin Toniazzo <jt@thoughtbot.com>
2026-06-27 15:06:38 -0400

    Add Galleries::RemoteVideoDownloadRowComponent

--- spec/factories/galleries/remote_video_downloads.rb
+++ spec/factories/galleries/remote_video_downloads.rb
@@ -4,5 +4,15 @@ FactoryBot.define do
     gallery
     sequence(:url) { "https://example.com/video/#{it}.mp4" }
     status { "pending" }
+
+    trait :completed do
+      status { "completed" }
+      image { association(:galleries_image, gallery:) }
+    end
+
+    trait :failed do
+      status { "failed" }
+      error_message { "unable to download video" }
+    end
   end
 end
