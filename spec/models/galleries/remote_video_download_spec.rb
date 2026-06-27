10acd833241039ad78e71d580fcd4426001802fb
Justin Toniazzo <jt@thoughtbot.com>
2026-06-27 15:46:52 -0400

    Add RemoteVideoDownload#broadcast_row

--- spec/models/galleries/remote_video_download_spec.rb
+++ spec/models/galleries/remote_video_download_spec.rb
@@ -21,4 +21,28 @@ RSpec.describe Galleries::RemoteVideoDownload do
   it "has a valid factory" do
     expect(build(:galleries_remote_video_download)).to be_valid
   end
+
+  it "broadcasts a replace of its row to the gallery stream" do
+    rvd = build_stubbed(:galleries_remote_video_download)
+    allow(Turbo::StreamsChannel).to receive(:broadcast_replace_to)
+
+    rvd.broadcast_row
+
+    expect(Turbo::StreamsChannel)
+      .to have_received(:broadcast_replace_to)
+      .with(
+        rvd.gallery.remote_video_downloads_stream_name,
+        target: "remote_video_download_#{rvd.id}",
+        partial: "galleries/remote_video_downloads/row",
+        locals: {remote_video_download: rvd}
+      )
+  end
+
+  it "swallows broadcast errors so callers are unaffected" do
+    rvd = build_stubbed(:galleries_remote_video_download)
+    allow(Turbo::StreamsChannel).to receive(:broadcast_replace_to)
+      .and_raise(StandardError.new("boom"))
+
+    expect { rvd.broadcast_row }.not_to raise_error
+  end
 end
