faf3451a399d2a9f2e49ef43d1d97a050ba630b1
Justin Toniazzo <jt@thoughtbot.com>
2026-09-15 14:31:55 -0400

    HPRB-97 Raise Todo records to typed: strict
    Linear-issue: Fixes HPRB-97
    Linear-issue-url: https://linear.app/jt-project42/issue/HPRB-97/raise-todo-records-to-typed-strict

--- spec/models/todo/task_spec.rb
+++ spec/models/todo/task_spec.rb
@@ -27,4 +27,31 @@ RSpec.describe Todo::Task do
   it "has a valid factory" do
     expect(build(:todo_task)).to be_valid
   end
+
+  describe "scheduling" do
+    it "is available when it has no scheduled occurrence" do
+      task = create(:todo_task)
+
+      expect(task).to be_available_for_scheduling
+      expect(task).not_to have_active_occurrence
+      expect(task.current_occurrence).to be_nil
+      expect(task.status).to eq("available")
+    end
+
+    it "returns its scheduled and latest completed occurrences" do
+      task = create(:todo_task)
+      earlier = create(:todo_task_occurrence, :completed, todo_task: task,
+        completed_at: 2.hours.ago)
+      later = create(:todo_task_occurrence, :completed, todo_task: task,
+        completed_at: 1.hour.ago)
+      scheduled = create(:todo_task_occurrence, todo_task: task)
+
+      expect(task).not_to be_available_for_scheduling
+      expect(task).to have_active_occurrence
+      expect(task.current_occurrence).to eq(scheduled)
+      expect(task.last_completed_occurrence).to eq(later)
+      expect(task.last_completed_occurrence).not_to eq(earlier)
+      expect(task.status).to eq("scheduled")
+    end
+  end
 end
